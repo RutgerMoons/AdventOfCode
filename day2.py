52f06b97e7e7f89492883cdd52e84ec000dbb98f
Rutger Moons <rutger.moons@technicolor.com>
2018-12-06 10:38:41 +0100

    Finish day 2
    * Clean up code
    * Clean up input at the start (not only for part 2)
    * Finish part 2

--- day2.py
+++ day2.py
@@ -1,5 +1,5 @@
 with open('input/day2') as input:
-	vals = input.readlines()
+	vals = list(map(lambda x: x.strip(), input.readlines()))
 
 # part 1
 counter2, counter3 = 0, 0
@@ -13,35 +13,22 @@ for val in vals:
 	if found2: counter2 += 1
 	if found3: counter3 += 1
 
-print(counter2 * counter3)
+print("Part 1: %d" % (counter2 * counter3))
 
 #part 2
-def differ_by_one(t1, t2):
-	s1, s2 = t1[0], t2[0]
-	if len(s1) != len(s2): return False
-	if len(s1 & s2) != len(s1) - 1:
-		return False
-	
-	count = 0
-	str1, str2 = t1[1], t2[1]
-	for i in range(len(str1)):
-		if str1[i] != str2[i]:
-			count += 1
-	return count == 1
+def differ_by_one(fst, sec):
+	if len(fst) != len(sec): return False
 
-#vals = ['abcde', 'fghij','klmno','pqrst','fguij','axcye','wvxyz']
-sets = list(map(lambda x: (set(x.strip()), x.strip()), vals))
+	for i in range(len(fst)):
+		if fst[i] != sec[i]:
+			return fst[i+1:] == sec[i+1:], fst[:i] + fst[i+1:]
+	return False, None
 
-def find_one(sets):
-	for i in range(len(sets) - 1):
-		for j in range(i + 1, len(sets)):
-			if differ_by_one(sets[i], sets[j]):
-				print ('Found one')
-				badChar = list(sets[i][0] - (sets[i][0] & sets[j][0]))[0]
-				print (sets[i])
-				print (sets[j])
-				print(badChar)
-				print(sets[i][1].replace(badChar, ''))
+def find_one(vals):
+	for i in range(len(vals) - 1):
+		for j in range(i + 1, len(vals)):
+			differ, result = differ_by_one(vals[i], vals[j])
+			if differ:
+				return result
 
-find_one(sets)
-#print(find_one(sets))
+print("Part 2: %s" % find_one(vals))
