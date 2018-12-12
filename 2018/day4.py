c7d21ca49f61e3f0abbebc7f8e93a5b4923cc1b4
Rutger Moons <rutger.moons@technicolor.com>
2018-12-12 13:27:59 +0100

    solve day 4

--- 2018/day4.py
+++ 2018/day4.py
@@ -19,7 +19,24 @@ class Action:
 	def __str__(self):
 		return "[%d-%d-%d %d:%d]%s" % (self.year, self.month, self.day, self.hour, self.minute, self.event)
 
-with open('input/test4') as input:
+class Guard:
+	def __init__(self, number):
+		self.minutes = [0] * 60
+		self.start_sleep = None
+		self.number = number
+
+	def total_sleep(self):
+		return sum(self.minutes)
+
+	def max_minute(self):
+		idx, val = -1, -1
+		for i in range(len(self.minutes)):
+			if self.minutes[i] > val:
+				idx = i
+				val = self.minutes[i]
+		return idx, val
+
+with open('input/day4') as input:
 	vals = list(map(lambda x: x.strip(), input.readlines()))
 
 actions = []
@@ -27,7 +44,6 @@ for val in vals:
 	pattern = '\[(\d+)-(\d+)-(\d+)\s(\d+):(\d+)\](.*)'
 	comp = re.compile(pattern)
 	match = comp.match(val)
-	print(match.group(0))
 	year = int(match.group(1))
 	month = int(match.group(2))
 	day = int(match.group(3))
@@ -38,6 +54,46 @@ for val in vals:
 
 actions = sorted(actions, key=lambda x: (x.year, x.month, x.day, x.hour, x.minute))
 
-#TODO: process actions
-# Create an hours array per guard, increment minute entry per minute asleep
-# find the max sleep and times that by the max minute
+guards = {}
+current_guard = -1
+
+pattern = '.*#(\d+).*'
+compilation = re.compile(pattern)
+for ac in actions:
+	if '#' in ac.event:
+		match = compilation.match(ac.event)
+		number = int(match.group(1))
+		current_guard = number
+		if guards.get(current_guard, None) is None:
+			guards[current_guard] = Guard(current_guard)
+	else:
+		if 'sleep' in ac.event:
+			guards.get(current_guard).start_sleep = ac.minute
+		else:
+			g = guards.get(current_guard)
+			start_sleep = g.start_sleep
+			end_sleep = ac.minute
+
+			for i in range(end_sleep - start_sleep):
+				g.minutes[i + start_sleep] += 1
+
+max_guard = None
+max_sleep = -1
+for guard in guards.values():
+	if guard.total_sleep() > max_sleep:
+		max_sleep = guard.total_sleep()
+		max_guard = guard
+
+print("Part 1: guard number: %d, max minute: %d, answer: %d" % (max_guard.number, max_guard.max_minute()[0], max_guard.number * max_guard.max_minute()[0]))
+
+max_guard = None
+max_minute_count = -1
+max_minute_idx = -1
+for guard in guards.values():
+	max_minute, val = guard.max_minute()
+	if val > max_minute_count:
+		max_guard = guard
+		max_minute_count = val
+		max_minute_idx = max_minute
+
+print("Part 2: guard number: %d, max minute: %d, answer: %d" % (max_guard.number, max_minute_idx, max_guard.number * max_minute_idx))
