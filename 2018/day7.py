e1aed4208c96dab41fd51d23b6b7d93cf9e04e9b
RutgerMoons <rutgermoons1@hotmail.com>
2018-12-13 13:36:53 +0100

    day 7 part 1

--- 2018/day7.py
+++ 2018/day7.py
@@ -1,7 +1,20 @@
 #! /usr/bin/env python3
 import re
 
-with open('input/test7') as input:
+class Node:
+	def __init__(self, name):
+		self.name = name
+		self.children = []
+		self.parent = []
+
+	def __str__(self):
+		children = ", ".join(self.children)
+		return "%s: %s" % (self.name, children)
+
+	def __repr__(self):
+		return self.__str__()
+
+with open('input/day7') as input:
 	lines = list(map(lambda x: x.strip(), input.readlines()))
 
 pattern = 'Step\s(.).*step\s(.).*'
@@ -11,5 +24,36 @@ for line in lines:
 	match = comp.match(line)
 	deps.append((match.group(1), match.group(2)))
 
-print(deps)
+
+nodes = {}
+for d in deps:
+	p = d[0]
+	c = d[1]
+	pnode = nodes.get(p, None)
+	cnode = nodes.get(c, None)
+	if not pnode:
+		pnode = Node(p)
+		nodes[p] = pnode
+	if not cnode:
+		cnode = Node(c)
+		nodes[c] = cnode
+	pnode.children.append(c)
+	cnode.parent.append(p)
+
+vals = nodes.values()
+result = ""
+while len(vals) > 0:
+	roots = []
+	for n in vals:
+		if not n.parent:
+			roots.append(n)
+	res = sorted(roots, key=lambda x: x.name)[0]
+	result += res.name
+	for c in res.children:
+		nodes.get(c).parent.remove(res.name)
+	del nodes[res.name]
+	vals = nodes.values()
+
+print("Part 1: %s" % result)
+
 
