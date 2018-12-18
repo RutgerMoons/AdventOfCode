5da97ea6c863d742687b3c7daadaa4c593e6cae4
Rutger Moons <rutgermoons1@hotmail.com>
2018-12-18 12:24:21 +0100

    Solve day 11 part 2
    Use memoisation to not calculate the same grid of numbers every time

--- 2018/day11.py
+++ 2018/day11.py
@@ -25,8 +25,51 @@ def calc_total_power(board, grid_size):
 				top_x, top_y = x + 1, y + 1
 	return top_x, top_y, max_power
 
-serial = 18
-grid_size = 3
+def calc_dynamic(board, x, y, g, sums):
+	bad_val = None
+	get = sums.get((x,y,g), bad_val)
+	if get != bad_val:
+		return get
+	
+	if g == 1:
+		val = board[x][y]
+		sums[(x,y,1)] = val
+		return val
+	
+	val = calc_dynamic(board, x + 1, y + 1, g - 1, sums) + sum(board[x][y:y+g])
+	for i in range(1, g):
+		val += board[x+i][y]
+	sums[(x, y, g)] = val
+	return val
+
+def calc_all_sums(board):
+	sums = {}
+	for g in range(1, len(board) + 1):
+		print("g: {}".format(g))
+		for x in range(len(board) + 1 - g):
+			for y in range(len(board) + 1 - g):
+				calc_dynamic(board, x, y, g, sums)
+	return sums
+
+def calc_total_power_variable_grid_size(board):
+	top_x, top_y = 1, 1
+	max_power = -100
+	max_grid = 1
+	sums = calc_all_sums(board)
+
+	for g in range(1, len(board) + 1):
+		for x in range(len(board)):
+			for y in range(len(board)):
+				p = sums.get((x, y, g), None)
+				if p == None:
+					continue
+				if p > max_power:
+					max_power = p
+					top_x, top_y = x + 1, y + 1
+					max_grid = g
+	return top_x, top_y, max_grid, max_power
+
+serial = 6303
 board_size = 300
 board = []
 for i in range(board_size):
@@ -36,5 +79,9 @@ for x in range(board_size):
 	for y in range(board_size):
 		board[x][y] = calc_power_cell(x + 1, y + 1, serial)
 
-top_x, top_y, max_power = calc_total_power(board, grid_size)
-print("Top coordinate: {}, {} with power {}".format(top_x, top_y, max_power))
+
+top_x, top_y, max_power = calc_total_power(board, 3)
+print("Part 1: Top coordinate: {}, {} with power {}".format(top_x, top_y, max_power))
+
+top_x, top_y, max_grid, max_power = calc_total_power_variable_grid_size(board)
+print("Part 2: Top coordinate: {}, {} with power {}, for grid size {}".format(top_x, top_y, max_power, max_grid))
