c299816230c5c54cd35b058711b9190c0b1c12c2
Rutger Moons <rutgermoons1@hotmail.com>
2018-12-20 13:00:08 +0100

    solve day 14 part 1

---
+++ 2018/day14.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+
+elfs = [0, 1]
+nb_recipes = 919901
+extra_scores = 10
+extra_safety = 5 * len(elfs)
+
+scores = [0] * (nb_recipes + extra_scores + extra_safety)
+scores[0] = 3
+scores[1] = 7
+nb_scores = 2
+
+
+
+def step(scores, nb_scores, elfs):
+	total = 0
+	for elf_idx in elfs:
+		total += scores[elf_idx]
+
+	digits = []
+	if total == 0:
+		digits = [0]
+	else:
+		while total:
+			digits.append(total % 10)
+			total //= 10
+
+	for d in reversed(digits):
+		scores[nb_scores] = d
+		nb_scores += 1
+
+	new_elfs = []
+	for elf_idx in elfs:
+		new_idx = elf_idx + scores[elf_idx] + 1
+		new_idx %= nb_scores
+		new_elfs.append(new_idx)
+
+	return scores, nb_scores, new_elfs
+
+
+while nb_scores < nb_recipes + extra_scores:
+	scores, nb_scores, elfs = step(scores, nb_scores, elfs)
+
+print(''.join(list(map(str, scores[nb_recipes:nb_recipes + extra_scores]))))
