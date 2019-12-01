8fb52adadff9100b486a2a6aa4bfd820de952e45
RutgerMoons <rutgermoons1@hotmail.com>
2019-12-01 15:59:32 +0100

    day 1 part 2

--- 2019/day1/src/main.rs
+++ 2019/day1/src/main.rs
@@ -4,21 +4,25 @@ use std::path::Path;
 
 fn main() {
 	let mut total = 0;
+	let mut total_rec = 0;
 
     if let Ok(lines) = read_lines("/home/rutger/Programming/AdventOfCode/2019/input/day1_1") {
         for line in lines {
             if let Ok(input) = line {
 				let parsed = input.parse::<u32>();
 				let nb = match parsed {
-					Ok(x) => calculate_fuel_consumption(x),
+					Ok(x) => x,
 					Err(_) => 0
 				};
-				total += nb;
+
+				total += calculate_fuel_consumption(nb);
+				total_rec += calculate_fuel_consumption_rec(nb);
             }
         }
     }
 
-	println!("{}", total);
+	println!("Total {}", total);
+	println!("Total rec {}", total_rec);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -28,7 +32,19 @@ where P: AsRef<Path>, {
 }
 
 fn calculate_fuel_consumption(x: u32) -> u32 {
+	if x < 9 {
+		return 0;
+	}
 	x / 3 - 2
+}
+
+fn calculate_fuel_consumption_rec(x: u32) -> u32 {
+	if x < 9 {
+		return 0
+	}
+
+	let c = calculate_fuel_consumption(x);
+	c + calculate_fuel_consumption_rec(c)
 }
 
 #[cfg(test)]
@@ -53,5 +69,15 @@ mod tests {
     #[test]
     fn test_4() {
         assert_eq!(calculate_fuel_consumption(100756), 33583);
+    }
+
+    #[test]
+    fn test_5() {
+        assert_eq!(calculate_fuel_consumption_rec(1969), 966);
+    }
+
+    #[test]
+    fn test_6() {
+        assert_eq!(calculate_fuel_consumption_rec(100756), 50346);
     }
 }
